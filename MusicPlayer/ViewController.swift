b4ffc423a1400927b168c57686457b0d09299eab
Gregorius Yuristama <gregoriusyuristama@gmail.com>
2024-05-03 18:10:06 +0700

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Gregorius Yuristama Nugraha on 5/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
