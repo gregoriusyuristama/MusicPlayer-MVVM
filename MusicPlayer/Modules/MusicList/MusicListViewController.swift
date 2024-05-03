258398773f074a13fc7b342fbfaf67f26fe613ae
Gregorius Yuristama <gregoriusyuristama@gmail.com>
2024-05-03 18:47:03 +0700

    ref: refactor app to use uikit programatically

--- MusicPlayer/Modules/MusicList/MusicListViewController.swift
+++ MusicPlayer/Modules/MusicList/MusicListViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MusicListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
