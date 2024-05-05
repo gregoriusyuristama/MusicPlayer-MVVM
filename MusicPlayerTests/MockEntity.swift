ef4de799f329d3c1b938b91ff2f7a5009051b5ca
Gregorius Yuristama <gregoriusyuristama@gmail.com>
2024-05-05 14:03:59 +0700

    fix: fix model initialization in MockEntity and comment out unused UITest

--- MusicPlayerTests/MockEntity.swift
+++ MusicPlayerTests/MockEntity.swift
@@ -22,17 +22,17 @@ class MockEntity {
         .init(
             songName: "Song 1",
             artistName: "Artist 1",
-            audioUrl: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/ae/ab/1d/aeab1df3-29b8-e98d-04f2-c372d3aa7861/mzaf_8839786800171875369.plus.aac.p.m4a")
+            audioUrl: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/ae/ab/1d/aeab1df3-29b8-e98d-04f2-c372d3aa7861/mzaf_8839786800171875369.plus.aac.p.m4a"), songArtwork: nil
         ),
         .init(
             songName: "Song 2",
             artistName: "Artist 2",
-            audioUrl: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/25/dd/6a/25dd6ad6-39c6-32c3-c97e-50ec9422121e/mzaf_1878860000998513744.plus.aac.p.m4a")
+            audioUrl: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/25/dd/6a/25dd6ad6-39c6-32c3-c97e-50ec9422121e/mzaf_1878860000998513744.plus.aac.p.m4a"), songArtwork: nil
         ),
         .init(
             songName: "Song 3",
             artistName: "Artist 3",
-            audioUrl: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/50/f1/31/50f131e7-edb0-fb76-fa1f-8da6b78813b8/mzaf_4337393000404489784.plus.aac.p.m4a")
+            audioUrl: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/50/f1/31/50f131e7-edb0-fb76-fa1f-8da6b78813b8/mzaf_4337393000404489784.plus.aac.p.m4a"), songArtwork: nil
         ),
 
     ]
@@ -40,19 +40,19 @@ class MockEntity {
     static let singleMusic: Music = .init(
         songName: "Song 1",
         artistName: "Artist 1",
-        audioUrl: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/50/f1/31/50f131e7-edb0-fb76-fa1f-8da6b78813b8/mzaf_4337393000404489784.plus.aac.p.m4a")
+        audioUrl: URL(string: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/50/f1/31/50f131e7-edb0-fb76-fa1f-8da6b78813b8/mzaf_4337393000404489784.plus.aac.p.m4a"), songArtwork: nil
     )
     
     static let musicListBrokenUrl: [Music] = [
         .init(
             songName: "Broken Song 1",
             artistName: "Broken Artist 1",
-            audioUrl: URL(string: "Invalid URL")
+            audioUrl: URL(string: "Invalid URL"), songArtwork: nil
         ),
         .init(
             songName: "Broken Song 2",
             artistName: "Broken Artist 2",
-            audioUrl: URL(string: "Broken URL")
+            audioUrl: URL(string: "Broken URL"), songArtwork: nil
         )
     ]
 }
